68565c8aef0e22895f1b5ef64d8f2925ad6c85e6
Yashoda <yashodalakshmi999@gmail.com>
2023-11-10 20:56:21 +0530

    Nest one Array within Another Array

---
+++ 01 - Basic JavaScript/40-Nest-one-Array-within-Another-Array.js
@@ -0,0 +1,14 @@
+// 40. Nest one Array within Another Array
+// You can also nest arrays within other arrays, like below:
+
+// const teams = [["Bulls", 23], ["White Sox", 45]];
+// This is also called a multi-dimensional array.
+
+// 40. QUESTION :
+
+// Create a nested array called myArray.
+
+// SOLUTION :
+
+// Only change code below this line
+const myArray = [["SUNDAY", 1], ["MONDAY", 2], ["TUESDAY", 3], ["WEDNESDAY", 4], ["THURSDAY", 5], ["FRIDAY", 6], ["SATURDAY", 7]];
