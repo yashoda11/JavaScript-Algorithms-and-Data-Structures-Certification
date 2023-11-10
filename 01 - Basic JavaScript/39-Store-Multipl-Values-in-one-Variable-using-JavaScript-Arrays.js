931a4dae515b035102c460065d3e90925972892d
Yashoda <yashodalakshmi999@gmail.com>
2023-11-10 20:49:25 +0530

    Store Multiple Values in one Variable using JavaScript Arrays

---
+++ 01 - Basic JavaScript/39-Store-Multipl-Values-in-one-Variable-using-JavaScript-Arrays.js
@@ -0,0 +1,13 @@
+// 38. Store Multiple Values in one Variable using JavaScript Arrays
+// With JavaScript array variables, we can store several pieces of data in one place.
+
+// You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry, like this:
+
+// const sandwich = ["peanut butter", "jelly", "bread"];
+
+// 38. Question :
+
+// SOLUTION :
+
+// Only change code below this line
+const myArray = ["Sunday" , 1, "Monday", 2];
