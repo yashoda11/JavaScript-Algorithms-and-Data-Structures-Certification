8ab182a3862e12d49a642b4bca8d755a9a13f194
Yashoda <yashodalakshmi999@gmail.com>
2023-12-06 22:24:52 +0530

    Practice comparing different values

--- 01 - Basic JavaScript/62-Practice-comparing-different-values.js
+++ 01 - Basic JavaScript/62-Practice-comparing-different-values.js
@@ -3,8 +3,7 @@
 
 // If the values being compared are not of the same type, the equality operator will perform a type conversion, and then evaluate the values. However, the strict equality operator will compare both the data type and value as-is, without converting one type to the other.
 
-// Examples
-
+// Examples :
 // 3 == '3' returns true because JavaScript performs type conversion from string to number. 3 === '3' returns false because the types are different and type conversion is not performed.
 
 // Note: In JavaScript, you can determine the type of a variable or a value with the typeof operator, as follows:
@@ -13,9 +12,11 @@
 // typeof '3'
 // typeof 3 returns the string number, and typeof '3' returns the string string.
 
+// QUESTION :
 // The compareEquality function in the editor compares two values using the equality operator. Modify the function so that it returns the string Equal only when the values are strictly equal.
 
-// Setup
+
+// SOLUTION :
 function compareEquality(a, b) {
     if (a === b) { // Change this line
       return "Equal";
@@ -23,4 +24,4 @@ function compareEquality(a, b) {
     return "Not Equal";
   }
   
-  compareEquality(10, "10");
+console.log(compareEquality(10, "10"));
