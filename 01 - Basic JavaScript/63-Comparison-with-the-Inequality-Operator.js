150a6cf92983384225a0d82fb99a81af9bfcf6d7
Yashoda <yashodalakshmi999@gmail.com>
2023-12-06 22:26:39 +0530

    Comparison with the Inequality Operator

--- 01 - Basic JavaScript/63-Comparison-with-the-Inequality-Operator.js
+++ 01 - Basic JavaScript/63-Comparison-with-the-Inequality-Operator.js
@@ -1,16 +1,19 @@
-// Comparison with the Inequality Operator
+// Comparison with the Inequality Operator:
+
 // The inequality operator (!=) is the opposite of the equality operator. It means not equal and returns false where equality would return true and vice versa. Like the equality operator, the inequality operator will convert data types of values while comparing.
 
-// Examples
+// Examples :
 
 // 1 !=  2    // true
 // 1 != "1"   // false
 // 1 != '1'   // false
 // 1 != true  // false
 // 0 != false // false
+
+// QUESTION :
 // Add the inequality operator != in the if statement so that the function will return the string Not Equal when val is not equivalent to 99.
 
-// Setup
+// SOLUTION :
 function testNotEqual(val) {
     if (val != 99) { // Change this line
       return "Not Equal";
@@ -18,4 +21,4 @@ function testNotEqual(val) {
     return "Equal";
   }
   
-  testNotEqual(10);
+console.log(testNotEqual(10));
