80eddfebcda6a7fbb61ff22e6dae8ac148e2a3d4
Yashoda <yashodalakshmi999@gmail.com>
2023-12-06 22:30:52 +0530

    Comparison with the Greater Than Operator

--- 01 - Basic JavaScript/65-Comparison-with-the-Greater-Than-Operator.js
+++ 01 - Basic JavaScript/65-Comparison-with-the-Greater-Than-Operator.js
@@ -1,16 +1,20 @@
-// Comparison with the Greater Than Operator
+// Comparison with the Greater Than Operator :
+
 // The greater than operator (>) compares the values of two numbers. If the number to the left is greater than the number to the right, it returns true. Otherwise, it returns false.
 
 // Like the equality operator, the greater than operator will convert data types of values while comparing.
 
-// Examples
+// Examples :
 
 // 5   >  3  // true
 // 7   > '3' // true
 // 2   >  3  // false
 // '1' >  9  // false
+
+// QUESTION :
 // Add the greater than operator to the indicated lines so that the return statements make sense.
 
+// SOLUTION :
 function testGreaterThan(val) {
     if (val > 100) {  // Change this line
       return "Over 100";
@@ -22,5 +26,5 @@ function testGreaterThan(val) {
   
     return "10 or Under";
   }
-  
-  testGreaterThan(10);
+
+  console.log(testGreaterThan(10));
