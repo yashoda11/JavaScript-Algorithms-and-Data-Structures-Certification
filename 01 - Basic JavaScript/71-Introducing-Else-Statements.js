8dd492b6bd65ea2a586b24317a5838c404bebf72
Yashoda <yashodalakshmi999@gmail.com>
2024-01-06 23:23:21 +0530

    71-Introducing-Else-Statements.js

--- 01 - Basic JavaScript/71-Introducing-Else-Statements.js
+++ 01 - Basic JavaScript/71-Introducing-Else-Statements.js
@@ -1,8 +1,10 @@
 // 71 - Introducing Else Statements
+
 // When a condition for an if statement is true, the block of code following it is executed. What about when that condition is false? Normally nothing would happen. With an else statement, an alternate block of code can be executed.
 
 // if (num > 10) {
 //   return "Bigger than 10";
+
 // } else {
 //   return "10 or Less";
 // }
@@ -34,4 +36,3 @@ var result = testElse(val);
 console.log(`
 The given number ${val} is ${result}
 `);
-
