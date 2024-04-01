916098770b7f371c49b6578c1a0bc68a9f942e49
Yashoda <yashodalakshmi999@gmail.com>
2024-04-01 22:35:37 +0530

    Add New Properties to a JavaScript Object

--- 01 - Basic JavaScript/88-Add-New-to-a-JavaScript-Object.js
+++ 01 - Basic JavaScript/88-Add-New-to-a-JavaScript-Object.js
@@ -1,23 +1,33 @@
-Add New Properties to a JavaScript Object
-You can add new properties to existing JavaScript objects the same way you would modify them.
+// Add New Properties to a JavaScript Object
+// You can add new properties to existing JavaScript objects the same way you would modify them.
 
-Here's how we would add a bark property to ourDog:
+// Here's how we would add a bark property to ourDog:
 
-ourDog.bark = "bow-wow";
-or
+// ourDog.bark = "bow-wow";
+// or
 
-ourDog["bark"] = "bow-wow";
-Now when we evaluate ourDog.bark, we'll get his bark, bow-wow.
+// ourDog["bark"] = "bow-wow";
+// Now when we evaluate ourDog.bark, we'll get his bark, bow-wow.
 
-Example:
+// Example:
 
-const ourDog = {
-  "name": "Camper",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["everything!"]
-};
+// const ourDog = {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"]
+// };
 
-ourDog.bark = "bow-wow";
-Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+// ourDog.bark = "bow-wow";
+// Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
 
+// SOLUTION
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.bark = "woof"
