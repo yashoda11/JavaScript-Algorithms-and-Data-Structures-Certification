f36e2a59077877f485ffa922fcb3bef4db471f20
Yashoda <yashodalakshmi999@gmail.com>
2024-04-01 19:37:17 +0530

    Multiple Identical Options in Switch Statements

--- 01 - Basic JavaScript/78-Multiple-Identical-Options-in-Switch-Statements.js
+++ 01 - Basic JavaScript/78-Multiple-Identical-Options-in-Switch-Statements.js
@@ -20,3 +20,31 @@
 
 // Note: You will need to have a case statement for each number in the range.
 
+// SOLUTION :
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+  switch (val) {
+    case 1: 
+    case 2:
+    case 3:
+    answer = "Low";
+    break;
+    case 4:
+    case 5:
+    case 6:
+    answer="Mid";
+    break; 
+    case 7:
+    case 8:
+    case 9:
+    answer="High";
+    break; 
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  sequentialSizes(1);
