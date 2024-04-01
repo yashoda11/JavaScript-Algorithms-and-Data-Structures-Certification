722f32acb8f1d918c1d12fcb5737b8980428445e
Yashoda <yashodalakshmi999@gmail.com>
2024-04-01 22:38:12 +0530

    Delete Properties from a JavaScript Object

--- 01 - Basic JavaScript/89-Delete-Properties-from-a-JavaScript-Object.js
+++ 01 - Basic JavaScript/89-Delete-Properties-from-a-JavaScript-Object.js
@@ -1,25 +1,38 @@
-Delete Properties from a JavaScript Object
-We can also delete properties from objects like this:
+// Delete Properties from a JavaScript Object
+// We can also delete properties from objects like this:
 
-delete ourDog.bark;
-Example:
+// delete ourDog.bark;
+// Example:
 
-const ourDog = {
-  "name": "Camper",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["everything!"],
-  "bark": "bow-wow"
-};
+// const ourDog = {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"],
+//   "bark": "bow-wow"
+// };
 
-delete ourDog.bark;
-After the last line shown above, ourDog looks like:
+// delete ourDog.bark;
+// After the last line shown above, ourDog looks like:
 
-{
-  "name": "Camper",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["everything!"]
-}
-Delete the tails property from myDog. You may use either dot or bracket notation.
+// {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"]
+// }
+// Delete the tails property from myDog. You may use either dot or bracket notation.
 
+// SOLUTION
+
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails;
