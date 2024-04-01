6552cecaad8f5a3bb976e9718a6f6fc511adfca0
Yashoda <yashodalakshmi999@gmail.com>
2024-04-01 22:48:06 +0530

    Using Objects for Lookups

--- 01 - Basic JavaScript/90-Using-Objects-for-Lookups.js
+++ 01 - Basic JavaScript/90-Using-Objects-for-Lookups.js
@@ -20,4 +20,61 @@
 // articleAuthor is the string Kaashan Hussain, articleLink is the string https://www.freecodecamp.org/news/a-complete-guide-to-creating-objects-in-javascript-b0e2450655e8/, and valueLookup is the string How to create objects in JavaScript.
 
 // Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+    switch(val) {
+      case "alpha":
+        result = "Adams";
+        break;
+      case "bravo":
+        result = "Boston";
+        break;
+      case "charlie":
+        result = "Chicago";
+        break;
+      case "delta":
+        result = "Denver";
+        break;
+      case "echo":
+        result = "Easy";
+        break;
+      case "foxtrot":
+        result = "Frank";
+    }
+    
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
 
+// SOLUTION
+
+
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+   
+  
+    const lookup = {
+      alpha: "Adams",
+      bravo : "Boston",
+      charlie: "Chicago",
+      delta: "Denver",
+     echo : "Easy",
+     foxtrot : "Frank",
+    }
+  
+   result = lookup[val];
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
