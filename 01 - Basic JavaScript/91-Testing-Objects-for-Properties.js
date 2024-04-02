29a0705bbfa775a895c0d8dabfc93dd1fa29eb4a
Yashoda <yashodalakshmi999@gmail.com>
2024-04-02 18:21:20 +0530

    Testing Objects for Propertie

--- 01 - Basic JavaScript/91-Testing-Objects-for-Properties.js
+++ 01 - Basic JavaScript/91-Testing-Objects-for-Properties.js
@@ -11,5 +11,36 @@
 // checkForProperty({ top: 'hat', bottom: 'pants' }, 'middle'); // false
 // The first checkForProperty function call returns true, while the second returns false.
 
+// QUESTION
+
+// checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift") should return the string pony.
+// Passed:checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "pet") should return the string kitten.
+// Passed:checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "house") should return the string Not Found.
+// Passed:checkObj({city: "Seattle"}, "city") should return the string Seattle.
+// Passed:checkObj({city: "Seattle"}, "district") should return the string Not Found.
+// Passed:checkObj({pet: "kitten", bed: "sleigh"}, "gift") should return the string Not Found.
+
 // Modify the function checkObj to test if the object passed to the function parameter obj contains the specific property passed to the function parameter checkProp. If the property passed to checkProp is found on obj, return that property's value. If not, return Not Found.
 
+// SOLUTION
+
+var obj = {
+    gift: "pony", 
+    pet: "kitten", 
+    bed: "sleigh",
+    city: "Seattle"
+    }
+  
+  function checkObj(obj, checkProp) {
+    // Only change code below this line
+  
+  if (obj.hasOwnProperty(checkProp)){
+    return obj[checkProp];
+  }
+  else{
+    return "Not Found";
+  }
+        
+    // Only change code above this line
+  }
+  
