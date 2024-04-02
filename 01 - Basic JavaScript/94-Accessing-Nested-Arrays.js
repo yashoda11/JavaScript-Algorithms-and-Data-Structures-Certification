5fbdb93eaa880b361267a8a6c7c0c8f9cfe9f9a8
Yashoda <yashodalakshmi999@gmail.com>
2024-04-02 18:38:18 +0530

    Accessing Nested Arrays

---
+++ 01 - Basic JavaScript/94-Accessing-Nested-Arrays.js
@@ -0,0 +1,30 @@
+// Accessing Nested Arrays
+// As we have seen in earlier examples, objects can contain both nested objects and nested arrays. Similar to accessing nested objects, array bracket notation can be chained to access nested arrays.
+
+// Here is an example of how to access a nested array:
+
+// const ourPets = [
+//   {
+//     animalType: "cat",
+//     names: [
+//       "Meowzer",
+//       "Fluffy",
+//       "Kit-Cat"
+//     ]
+//   },
+//   {
+//     animalType: "dog",
+//     names: [
+//       "Spot",
+//       "Bowser",
+//       "Frankie"
+//     ]
+//   }
+// ];
+
+// ourPets[0].names[1];
+// ourPets[1].names[0];
+// ourPets[0].names[1] would be the string Fluffy, and ourPets[1].names[0] would be the string Spot.
+
+// Using dot and bracket notation, set the variable secondTree to the second element in the list array from the second object in the myPlants array.
+
